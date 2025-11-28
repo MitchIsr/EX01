0bc37919adfea92199962728e3d56a0359d85f9f
MitchIsr <m.shot751@gmail.com>
2025-11-28 06:40:10 -0500

    added main to different cs file

--- Ex01_01/BinaryNumber.cs
+++ Ex01_01/BinaryNumber.cs
@@ -94,11 +94,11 @@ namespace Ex01_01
             string l_Output = @" (";
             for (int i = m_BinaryNum.Length - 1; i >= 0; i--)
             {
-               l_Output = string.Concat(l_Output ,m_BinaryNum[i].ToString());
+                l_Output = string.Concat(l_Output, m_BinaryNum[i].ToString());
             }
-            l_Output = string.Concat(l_Output,") ");
+            l_Output = string.Concat(l_Output, ") ");
             Console.Write(l_Output);
-           
+
         }
         static void PrintBinaryNumber(int[,] i_Binary_Numbers, int row)
         {
@@ -146,7 +146,7 @@ namespace Ex01_01
         public bool IsPalindrome()
         {
             bool l_flag = true;
-            for (int i = 0, j = 7; i < 4; i++, j--) 
+            for (int i = 0, j = 7; i < 4; i++, j--)
             {
                 if (GetBinaryDigit(i) != GetBinaryDigit(j))
                 {
@@ -161,7 +161,7 @@ namespace Ex01_01
             int l_0sCounter = 0;
             for (int i = 0; i < 8; i++)
             {
-                if(GetBinaryDigit(i)== 1)
+                if (GetBinaryDigit(i) == 1)
                 {
                     l_1sCounter++;
                 }
@@ -175,66 +175,11 @@ namespace Ex01_01
         }
         public bool IsStartAndEndWithSameDigit()
         {
-            return GetBinaryDigit(0) == GetBinaryDigit(7); 
+            return GetBinaryDigit(0) == GetBinaryDigit(7);
         }
     }
 
-   
-    class main
-    {
-        static void Main()
-        {
-            BinaryNumber[] l_BinaryNumberArray = new BinaryNumber[3];
-    
-            string Output = @"Hello, Welcome to Ex_01_01 Binary Sequences
-Please Enter 3 Numbers In Binary Format With a Length of up to 8:";
-            Console.WriteLine(Output);
-            for (int i = 0; i < 3; i++)
-            {
-                l_BinaryNumberArray[i] = new BinaryNumber();
-                bool f_isInputValid = false;
-                while (f_isInputValid == false)
-                {
-                    string input = Console.ReadLine();
-                    try
-                    {
-                        l_BinaryNumberArray[i] = l_BinaryNumberArray[i].Parse(input);
-                        f_isInputValid = true;
-                    }
-                    catch  (Exception l_invalidinput)
-                    {
-                        Console.WriteLine(l_invalidinput.Message);
-                    }
-                }
-            }
-            BinaryNumberUtilities.quickSort( l_BinaryNumberArray, 0, l_BinaryNumberArray.Length - 1);
-            Output = "Decimal numbers in ascending order: ";
-            Console.Write(Output);
-            for (int i = 0; i < 3; i++)
-            {
-                l_BinaryNumberArray[i].PrintNumericValue();
-                l_BinaryNumberArray[i].PrintBinary();
-                if (i < 2) 
-                {
-                    Console.Write(", ");
-                }
-                
-            }
-            float l_BinaryNumbersAverage = BinaryNumberUtilities.Avg(l_BinaryNumberArray[0], l_BinaryNumberArray[1], l_BinaryNumberArray[2]);
-            Console.WriteLine();
-            Output = $@"Average: {l_BinaryNumbersAverage}";
-            Console.WriteLine(Output);
-            BinaryNumberUtilities.FindShortestBitSequence(l_BinaryNumberArray);
-            Console.WriteLine();
-            BinaryNumberUtilities.PrintPalindromesOfBinary(l_BinaryNumberArray);
-            Console.WriteLine();
-            BinaryNumberUtilities.PrintMaxDifferenceBinary(l_BinaryNumberArray);
-            Console.WriteLine();
-            BinaryNumberUtilities.PrintNumbersThatStartAndEndWithSameDigit(l_BinaryNumberArray);
-            Console.WriteLine();
 
-
-        }
-    }
 }
+
 
